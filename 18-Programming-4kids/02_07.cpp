33874cd8239397c4e13e2324ec5a1e4a9215681c
mostafa-saad <mostafa.saad.fci@gmail.com>
2020-03-02 19:56:55 -0800

    local

---
+++ 18-Programming-4kids/02_07.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"I am Mostafa";
+	return 0;
+
+	cout<<endl;
+	cout<<"I was born in Giza\n";
+	cout<<"I graduated from Cairo university";
+	cout<<endl;
+}
