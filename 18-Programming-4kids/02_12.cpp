510550d16d6203d63a6acd348971c0d80d9f279c
mostafa-saad <mostafa.saad.fci@gmail.com>
2020-03-06 07:26:26 -0800

    local

---
+++ 18-Programming-4kids/02_12.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+// Go over code line by line!
+
+int main()
+{
+	cout<<"If your dream only includes you, it’s too small\n\n";
+
+	cout<<30+20+10-10-20-30<<endl;
+	cout<<"70/10"<<"\n";
+	cout<<80/10/2<<"\n";
+
+	cout<<"\nOpportunities don't happen. "
+		<<"You create them"
+		<<endl;
+
+	// All progress takes place outside the comfort zone.
+
+	/*
+		Character cannot be developed in ease and quiet. Only through
+			experience of trial and suffering can the soul be strengthened,
+				ambition inspired, and success achieved
+	 */
+
+	return 0;
+
+	cout<<"\n\nDo one thing every day that scares you";
+}
--- 18-Programming-4kids/02_homework1.cpp
+++ 18-Programming-4kids/02_homework1.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main()
 {
-	cout<<"\n\nGuess the output\n---\n";
-	cout<<10+20-5<<endl;
+	cout<<"\n\nGuess the "
+		<<"output\n---\n";
 
+	cout<<10+20-5<<endl;
 	cout<<"14/2"<<"\n";
 	cout<<17-10<<"\n";
 	cout<<"endl";
 	cout<<"\nPractice makes perfect";
 
+	//cout<<"The way to get started is to quit talking and begin doing";
 
 	return 0;
 
