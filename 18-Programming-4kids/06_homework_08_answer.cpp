6c68b29d060348fa974c1587c427ccfe9f97a702
mostafa-saad <mostafa.saad.fci@gmail.com>
2020-03-18 09:23:56 -0700

    local

---
+++ 18-Programming-4kids/06_homework_08_answer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int days;
+	while(true) {
+	cin >> days;
+
+	int years = days / 360;
+	days = days % 360;	// now we remove # of complete years
+
+	int months = days / 30;
+	days = days % 30;
+
+	cout<<years<<" "<<months<<" "<<days<<"\n";
+	}
+
+	return 0;
+}
+
