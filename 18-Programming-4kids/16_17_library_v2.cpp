dd803ed79cb519762165a194073fe47f279d754c
mostafa-saad <mostafa.saad.fci@gmail.com>
2020-09-01 09:39:42 -0700

    local

--- 18-Programming-4kids/16_17_library_v2.cpp
+++ 18-Programming-4kids/16_17_library_v2.cpp
@@ -140,19 +140,17 @@ struct library_system {
 	int menu() {
 		int choice = -1;
 		while (choice == -1) {
-			if (false) {
-				cout << "\nLibrary Menu;\n";
-				cout << "1) add_book\n";
-				cout << "2) search_books_by_prefix\n";
-				cout << "3) print_who_borrowed_book_by_name\n";
-				cout << "4) print_library_by_id\n";
-				cout << "5) print_library_by_name\n";
-				cout << "6) add_user\n";
-				cout << "7) user_borrow_book\n";
-				cout << "8) user_return_book\n";
-				cout << "9) print_users\n";
-				cout << "10) Exit\n";
-			}
+			cout << "\nLibrary Menu;\n";
+			cout << "1) add_book\n";
+			cout << "2) search_books_by_prefix\n";
+			cout << "3) print_who_borrowed_book_by_name\n";
+			cout << "4) print_library_by_id\n";
+			cout << "5) print_library_by_name\n";
+			cout << "6) add_user\n";
+			cout << "7) user_borrow_book\n";
+			cout << "8) user_return_book\n";
+			cout << "9) print_users\n";
+			cout << "10) Exit\n";
 
 			cout << "\nEnter your menu choice [1 - 10]: ";
 			cin >> choice;
